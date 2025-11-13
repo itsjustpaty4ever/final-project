30b0f0e6773e74115ba7f88cb1976d42b1d43c2a
paty3 <itsjustpaty4ever@gmail.com>
2025-11-13 18:08:15 +0000

    day 1 of final project

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("code running");
+
+//creating an array of questions(ai helped with the layout and i edited the questions)
+let questions = [
+    {
+         question: "What is the capital city of France?",
+         options : ["Lille" , "Paris" , "Nice","Bourges"],
+         correct: 1
+    },
+    {
+         question: "how many continets are there?",
+         options : ["9" , "4" , "13","7"],
+         correct: 3
+    }
+]
+
+//next i will begin to take the information from inside the array questions and print it to the live screen
+
+
